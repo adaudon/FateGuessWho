9a5afc02f8add2a47f20ebefcf8e7c349263eed3
adaudon <tamoor_shahid@hotmail.com>
2025-07-27 15:38:00 +0100

    Created Fate Website Template

---
+++ src/data/servants.js
@@ -0,0 +1,23 @@
+const servants = [
+  {
+    id: 1,
+    name: "Artoria Pendragon",
+    class: "Saber",
+    gender: "Female",
+    hairColor: "Blonde",
+    alignment: "Lawful Good",
+    image: "/images/artoria.png",
+  },
+  {
+    id: 2,
+    name: "Gilgamesh",
+    class: "Archer",
+    gender: "Male",
+    hairColor: "Blonde",
+    alignment: "Chaotic Good",
+    image: "/images/gilgamesh.png",
+  },
+  // Add more servants!
+];
+
+export default servants;
